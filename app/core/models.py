34c004a70c0c25931084d6dd72f738de26f0da61
moein <mohammadmoein.moradi@gmail.com>
2021-09-03 16:11:40 +0430

    add Profile model

---
+++ app/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+import os
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    picture = models.CharField(max_length=255,
+                               default=os.environ.get('DEFAULT_PROFILE_PIC'))
+    about = models.CharField(max_length=1023, blank=True)
