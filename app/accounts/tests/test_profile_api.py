78c98fbbfcab3cc4dd1546a3a5b35119d19697aa
moein <mohammadmoein.moradi@gmail.com>
2021-09-05 09:01:39 +0430

    test updating user does not assign new profile

--- app/accounts/tests/test_profile_api.py
+++ app/accounts/tests/test_profile_api.py
@@ -15,3 +15,15 @@ class TestUser(TestCase):
         user_profile = Profile.objects.filter(user=user.id)
         self.assertEqual(1, len(user_profile))
         self.assertTrue(user_profile.exists())
+
+    def test_updating_user_doesnt_create_new_profile(self):
+        user = User.objects.create_user('test_name',
+                                        email='test_email@test.com',
+                                        password='test_pass')
+        updated_name = 'test_name_updated'
+        user.username = updated_name
+        user.save()
+
+        user_profile = Profile.objects.filter(user=user.id)
+        self.assertEqual(1, len(user_profile))
+        self.assertTrue(user_profile.exists())
