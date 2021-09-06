a1dcd1074e2344f8bf877644900a0410ff9577f9
moein <mohammadmoein.moradi@gmail.com>
2021-09-06 15:03:32 +0430

    add LogoutUserView

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from accounts import serializers
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -23,3 +23,14 @@ class LoginUserView(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutUserView(APIView):
+    """Handle user logout"""
+
+    def post(self, request):
+        if request.user:
+            logout(request)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
