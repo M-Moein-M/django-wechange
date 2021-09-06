07c8f67ea632294b76409d85245082694d23352f
moein <mohammadmoein.moradi@gmail.com>
2021-09-06 15:02:25 +0430

    add user-logout endpoint

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -5,4 +5,5 @@ from accounts import views
 urlpatterns = [
     path('create', views.CreateUserView.as_view(), name='user-create'),
     path('login', views.LoginUserView.as_view(), name='user-login'),
+    path('logout', views.LogoutUserView.as_view(), name='user-logout'),
 ]
