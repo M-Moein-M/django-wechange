91a6d6a4bc4606396154835a47031e35bb88c285
moein <mohammadmoein.moradi@gmail.com>
2021-09-06 14:35:12 +0430

    add LoginUserView

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,7 +1,25 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from accounts import serializers
+from django.contrib.auth import authenticate, login
 
 
 class CreateUserView(generics.CreateAPIView):
     """Handle user objects"""
     serializer_class = serializers.UserSerializer
+
+
+class LoginUserView(APIView):
+    """Handle user login"""
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
