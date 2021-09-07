b804d7877835a2fecdd777269b75f729ba61eb06
moein <mohammadmoein.moradi@gmail.com>
2021-09-07 09:56:34 +0430

    test updating profile

--- app/accounts/tests/test_profile_api.py
+++ app/accounts/tests/test_profile_api.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from core.models import Profile
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 
 import accounts.signals.profile_assignment_signal
 
@@ -27,3 +30,31 @@ class TestUser(TestCase):
         user_profile = Profile.objects.filter(user=user.id)
         self.assertEqual(1, len(user_profile))
         self.assertTrue(user_profile.exists())
+
+
+class TestProfileApi(TestCase):
+    """Test profile api"""
+
+    def get_profile_detail_url(self, pk):
+        return reverse('profile-detail', kwargs={'pk': pk})
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user('test_name',
+                                             email='test_email@test.com',
+                                             password='test_pass')
+
+    def test_profile_update(self):
+        self.assertTrue(Profile.objects.filter(user=self.user),
+                        msg='Creating new user must assign profile to it')
+        user_profile = Profile.objects.get(user=self.user)
+        payload = {
+            'about': 'Updated about-section of profile'
+        }
+        res = self.client.patch(self.get_profile_detail_url(user_profile.id),
+                                payload)
+        user_profile.refresh_from_db()
+
+        self.assertEqual(status.HTTP_200_OK, res.status_code)
+        for attr, value in payload.items():
+            self.assertEqual(value, getattr(user_profile, attr))
