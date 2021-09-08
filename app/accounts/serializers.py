f602c2e46949ff85430934a954c86f4b57a42a86
moein <mohammadmoein.moradi@gmail.com>
2021-09-08 09:47:50 +0430

    add updating user fields in ProfileSerializer

--- app/accounts/serializers.py
+++ app/accounts/serializers.py
@@ -8,16 +8,27 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
-        read_only_fields = ['id', 'password']
+        fields = ['id', 'username', 'email']
+        read_only_fields = ['id']
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Serializer for Profile model"""
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(),
-                                              required=False)
+    user = UserSerializer()
 
     class Meta:
         model = Profile
         fields = '__all__'
         read_only_fields = ['id', 'picture']
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.get('user')
+        if user_data:
+            user = instance.user
+            user.username = user_data.get('username', user.username)
+            user.email = user_data.get('email', user.email)
+            user.save()
+
+        instance.about = validated_data.get('about', instance.about)
+        instance.save()
+        return instance
