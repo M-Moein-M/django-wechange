3ee9fc69811595a4218b9779df118d7f2691a2a8
moein <mohammadmoein.moradi@gmail.com>
2021-09-10 09:52:59 +0430

    change user field to OneToOneField in Profile

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,7 @@ import os
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     picture = models.CharField(max_length=255,
                                default=os.environ.get('DEFAULT_PROFILE_PIC'))
     about = models.CharField(max_length=1023, blank=True)
