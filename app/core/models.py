e030e2e11e7b32d16a55a97055382b97c41c7b9c
moein <mohammadmoein.moradi@gmail.com>
2021-09-10 10:37:17 +0430

    add Skill model

--- app/core/models.py
+++ app/core/models.py
@@ -11,3 +11,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Skill(models.Model):
+    LEVEL_CHOICES = ((i, i) for i in range(10))
+
+    profile = models.ForeignKey(to=Profile,
+                                on_delete=models.CASCADE,
+                                related_name='skills',
+                                related_query_name='skill')
+    level = models.CharField(max_length=1,
+                             choices=LEVEL_CHOICES,
+                             default='0')
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{str(self.profile)}\t{self.name}\t{self.level}'
